24beb26e9a89cb7337cdd7329c69ff1544bfc8c7
PRO X <bredalisgautreaux@gmail>
2024-08-22 09:21:03 +0200

    chore: 🔧 Listo para desplegar

--- App.py
+++ App.py
@@ -1,15 +1,20 @@
 
 from flask import Flask, render_template
 
-app = Flask(__name__)
+def generador_musica()
 
-@app.route("/")
-def index():
-	return render_template("index.html")
+	app = Flask(__name__)
 
-@app.errorhandler(404)
-def error_404(e):
-	return render_template("404.html"), 404
+	@app.route("/")
+	def index():
+		return render_template("index.html")
+
+	@app.errorhandler(404)
+	def error_404(e):
+		return render_template("404.html"), 404
+
+	return app
 
 if __name__ == "__main__":
+	app = generador_musica()
 	app.run()
